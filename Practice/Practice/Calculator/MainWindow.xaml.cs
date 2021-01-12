9efc3a2e3e5fcab4a9121b0dee6a2395ca79c94f
NeoHSK <mistgrin@gmail.com>
2021-01-12 14:50:41 +0900

    Creating Event Handlers from XAML

--- Practice/Practice/Calculator/MainWindow.xaml.cs
+++ Practice/Practice/Calculator/MainWindow.xaml.cs
@@ -26,5 +26,18 @@ namespace Calculator
 
             resultLabel.Content = "1234";
         }
+
+        private void sevenButton_Click(object sender, RoutedEventArgs e)
+        {
+            if(resultLabel.Content.ToString() == "0")
+            {
+                resultLabel.Content = "7";
+            }
+            else
+            {
+                resultLabel.Content = $"{resultLabel.Content}7";
+            }
+        }
     }
+       
 }
