6247bb84e57061dcd2c20b44260a7c5d538c1414
NeoHSK <mistgrin@gmail.com>
2021-01-13 18:47:08 +0900

    Intersting into a Table

--- Practice/Practice/Contacts/NewContactWindow.xaml.cs
+++ Practice/Practice/Contacts/NewContactWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Contacts.Classes;
+using SQLite;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Windows;
@@ -24,6 +26,23 @@ namespace Contacts
 
         private void save_Click(object sender, RoutedEventArgs e)
         {
+            Contact contact = new Contact()
+            {
+                Name = nameTextBox.Text,
+                Email = emailTextBox.Text,
+                Phone = phoneTextBox.Text
+            };
+
+            string databaseName = "Contacts.db";
+            string folderPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            string databasePath = System.IO.Path.Combine(folderPath, databaseName);
+
+            SQLiteConnection connection = new SQLiteConnection(databasePath);
+
+            connection.CreateTable<Contact>();
+            connection.Insert(contact);
+            connection.Close();
+
             Close();
         }
     }
